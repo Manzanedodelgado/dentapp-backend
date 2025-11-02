c37f5df741c99143e857e80fae202d3606bffa3d
Manzanedodelgado <manzanedodelgado@gmail.com>
2025-11-02 05:46:04 +0100

    Initialize database package with MongoDB connection

---
+++ app/database/__init__.py
@@ -0,0 +1,13 @@
+"""
+Database package - MongoDB connection
+"""
+from .mongodb import mongodb, connect_to_mongo, close_mongo_connection, get_database
+# Crear alias 'db' para compatibilidad con el código existente
+db = mongodb.db
+__all__ = [
+    "mongodb",
+    "db",
+    "connect_to_mongo",
+    "close_mongo_connection",
+    "get_database"
+]
